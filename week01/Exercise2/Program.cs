878d9b6ce6878305813f702b3383670a6d2100a2
danielo2318 <aroctaviodaniel9@gmail.com>
2025-01-14 21:25:40 -0600

    excersises

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        Console.Write("Please enter your grade percentage: ");
+        string ValueFromUser= Console.ReadLine();
+        /*x = the percentage*/
+        int x = int.Parse(ValueFromUser);
+        
+        string letter ="";
+
+        if (x>=90)
+        {
+            letter = "A";
+        }
+        else if (x>=80)
+        {
+            letter = "B";
+        }
+        else if (x>=70)
+        {
+            letter = "C";
+        }
+        else if (x>=60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Ypur grade is: {letter}");
+
+        if (x>=70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+    } 
 }
